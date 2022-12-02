e4c08773b5d086223716352ae45cd6648060c54e
pi314ever <pilotflyer824@gmail.com>
2022-12-02 00:25:50 -0800

    Day 1 rust

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Day 1 code");
+    let contents = fs::read_to_string("day 1/input.txt")
+        .expect("Failed to read string.");
+    let contents_list: Vec<&str> = contents.split('\n').collect();
+    let mut calories = 0;
+    let mut totals = Vec::new();
+    for line in contents_list {
+        if line.is_empty() {
+            totals.push(calories);
+            calories = 0;
+            continue;
+        }
+        let calorie = line.parse::<u32>().expect("Line was not an integer");
+        calories += calorie;
+    }
+    totals.sort();
+    println!("Max calories count: {}", totals[totals.len() - 1]);
+    let top_3_sum: u32 = (totals[totals.len() - 3..])
+            .iter()
+            .sum();
+    println!("Total of top 3: {}", top_3_sum);
 }
